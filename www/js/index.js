ca2381bc18ddbded2a55479672563926e92d9ddb
Novalmedia <a.troitino@novalmedia.com>
2014-06-16 19:17:40 +0200

    marker

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+		FastClick.attach(document.body);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
