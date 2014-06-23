5e120b8071860dd6045dfe7ee8436f1a97b2caa8
Novalmedia <a.troitino@novalmedia.com>
2014-06-23 11:07:59 +0200

    asd

--- www/js/places.js
+++ www/js/places.js
@@ -199,16 +199,21 @@
 	
 	
 	var onSuccess = function(position) {
-    alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
-};
-
+		navigator.notification.alert(
+			'Latitude: '          + position.coords.latitude          + '\n' +
+			'Longitude: '         + position.coords.longitude         + '\n' +
+			'Altitude: '          + position.coords.altitude          + '\n' +
+			'Accuracy: '          + position.coords.accuracy          + '\n' +
+			'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+			'Heading: '           + position.coords.heading           + '\n' +
+			'Speed: '             + position.coords.speed             + '\n' +
+			'Timestamp: '         + position.timestamp                + '\n',  // message
+			alertDismissed,         // callback
+			'Game Over',            // title
+			'Done'                  // buttonName
+		);
+	};
+function alertDismissed(){}
 // onError Callback receives a PositionError object
 //
 function onError(error) {
