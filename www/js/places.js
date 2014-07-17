06ce146736b6741e0a3da960284fe04e438e71ae
Novalmedia <a.troitino@novalmedia.com>
2014-07-17 18:52:29 +0200

    lkj

--- www/js/places.js
+++ www/js/places.js
@@ -240,9 +240,9 @@ function getUserPosition(){
 
 function setupTable(tx){
 
-	tx.executeSql("DROP TABLE places");
+/* 	tx.executeSql("DROP TABLE places");
 	tx.executeSql("DROP TABLE people");
-	tx.executeSql("DROP TABLE profiles");
+	tx.executeSql("DROP TABLE profiles"); */
 	tx.executeSql("CREATE TABLE IF NOT EXISTS places(id INTEGER PRIMARY KEY,catid INTEGER,data)");
 	tx.executeSql("CREATE TABLE IF NOT EXISTS people(id INTEGER PRIMARY KEY,catid INTEGER,data)");
 	tx.executeSql("CREATE TABLE IF NOT EXISTS profiles(id INTEGER PRIMARY KEY,itemid INTEGER,data)");
