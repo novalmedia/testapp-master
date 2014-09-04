7032de0829234f8a13f6c4470874d4ba8070cbf5
Novalmedia <a.troitino@novalmedia.com>
2014-09-04 11:33:32 +0200

    asda

--- www/js/route.js
+++ www/js/route.js
@@ -126,7 +126,8 @@
 								]
 							  }
 							];
-	var deviceType = (navigator.userAgent.match(/iPad/i))  == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i))  == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) == "BlackBerry" ? "BlackBerry" : "null";
+	//var deviceType = (navigator.userAgent.match(/iPad/i))  == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i))  == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) == "BlackBerry" ? "BlackBerry" : "null";
+	var deviceType = "iPhone";
 	function initProfile() {
 	
 		var itemid = jQuery.getQuery('itemid');
